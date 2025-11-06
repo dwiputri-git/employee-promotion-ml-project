5f19de0cc60d586696d621349d2efe3509599dbc
dwiputri-git <dwip5704@gmail.com>
2025-11-06 23:22:22 +0700

    Update app.py

--- employee-promotion-app/app.py
+++ employee-promotion-app/app.py
@@ -7,7 +7,7 @@ import streamlit as st
 # Page configuration
 st.set_page_config(
     page_title="Employee Promotion Prediction",
-    page_icon="🚀",
+    page_icon="🧑‍💼",
     layout="wide",
     initial_sidebar_state="expanded"
 )
